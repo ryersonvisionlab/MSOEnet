da35b1e4853e39f659a73784e50b3addd2cbfc7c
Matthew Tesfaldet <mtesfald@scs.ryerson.ca>
2017-02-19 13:50:37 -0500

    changenames.sh script modified for better file naming

--- test_images/changenames.sh
+++ test_images/changenames.sh
@@ -2,4 +2,4 @@
 
 FOLDER=$1
 
-ls -v "$FOLDER" | cat -n | while read n f; do mv "$FOLDER/$f" "$FOLDER/frame_$n.jpeg" ; done
+ls -v "$FOLDER" | cat -n | while read n f; do mv "$FOLDER/$f" `printf "$FOLDER/frame_%08d.jpeg" $n` ; done
