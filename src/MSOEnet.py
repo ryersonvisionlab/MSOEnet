e3121f02ca656e53022f96c950d4ef44333af6a4
Matthew Tesfaldet <mtesfald@eecs.yorku.ca>
2017-03-31 22:32:06 -0500

    Adding extra l1 norm after first act in MSOEnet

--- src/MSOEnet.py
+++ src/MSOEnet.py
@@ -7,7 +7,6 @@ class MSOEnet(object):
     def __init__(self, name, input, reuse=None):
         self.name = name
         self.temporal_extent = 2
-        self.nbands = 2
         self.input_shape = input.get_shape().as_list()
 
         with tf.get_default_graph().name_scope(self.name):
@@ -19,11 +18,13 @@ class MSOEnet(object):
                            self.temporal_extent, 32, reuse)
             # activation
             h_conv1 = eltwise_square('square', conv1)
+            # channel-wise l1 normalization (batchx1xHxWx32)
+            l1_norm1 = l1_normalize('l1_norm1', h_conv1)
             # average pooling (1x3x3x1x1)
-            pool1 = avg_pool3d('avg_pool', h_conv1, 3, 1)
+            pool1 = avg_pool3d('avg_pool', l1_norm1, 3, 1)
             # second convolution (1x1x1x32x64)
             conv2 = conv3d('MSOEnet_conv2', pool1, 1, 1, 64, reuse)
             # channel-wise l1 normalization (batchx1xHxWx64)
-            l1_norm = l1_normalize('l1_norm', conv2)
+            l1_norm2 = l1_normalize('l1_norm2', conv2)
 
-            self.output = l1_norm
+            self.output = l1_norm2
