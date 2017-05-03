99d99e8e743c93c36a723f9ff91c7088a25d7e3d
Matthew Tesfaldet <mtesfald@eecs.yorku.ca>
2017-05-02 21:11:01 -0500

    training on Flying Chairs

--- train.py
+++ train.py
@@ -9,8 +9,8 @@ config_proto.log_device_placement = False
 # config.intra_op_parallelism_threads = 1
 my_config = {}
 my_config['train_filename'] = \
-    '/local/ssd/mtesfald/UCF-101-withflow'
-my_config['batch_size'] = 10
+    '/local/ssd/mtesfald/FlyingChairs/data'
+my_config['batch_size'] = 4
 my_config['iterations'] = 600000
 my_config['snapshot_frequency'] = 10000
 my_config['print_frequency'] = 10
@@ -18,8 +18,8 @@ my_config['validation_frequency'] = 500
 my_config['lr'] = 6e-3
 my_config['num_threads'] = 6
 my_config['num_scales'] = 5
-my_config['gpu'] = 0
-my_config['run_id'] = 'old_gating'
+my_config['gpu'] = 1
+my_config['run_id'] = 'old_gating_chairs'
 
 net = MSOEmultiscale(config={'tf': config_proto,
                              'user': my_config})
