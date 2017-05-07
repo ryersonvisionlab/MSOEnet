9db7314cad0ab80b9dfaa49c5777a3b83e78baca
Matthew Tesfaldet <mtesfald@eecs.yorku.ca>
2017-05-06 20:28:51 -0400

    Changed 5x5 max pool to 11x11 max pool.

--- src/MSOEnet.py
+++ src/MSOEnet.py
@@ -19,8 +19,8 @@ class MSOEnet(object):
                            self.temporal_extent, 32, reuse)
             # activation
             h_conv1 = eltwise_square('square', conv1)
-            # max pooling (1x5x5x1x1)
-            pool1 = max_pool3d('max_pool', h_conv1, 5, 1)
+            # max pooling (1x11x11x1x1)
+            pool1 = max_pool3d('max_pool', h_conv1, 11, 1)
             # second convolution (1x1x1x32x64)
             conv2 = conv3d('MSOEnet_conv2', pool1, 1, 1, 64, reuse)
             # activation
--- train.py
+++ train.py
@@ -18,8 +18,8 @@ my_config['validation_frequency'] = 500
 my_config['lr'] = 6e-3
 my_config['num_threads'] = 6
 my_config['num_scales'] = 5
-my_config['gpu'] = 0
-my_config['run_id'] = 'no_gating_deeperMSOEnet_1b_ucf101'
+my_config['gpu'] = 2
+my_config['run_id'] = 'no_gating_deeperMSOEnet_1c_ucf101'
 
 net = MSOEmultiscale(config={'tf': config_proto,
                              'user': my_config})
